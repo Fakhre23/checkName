21be266dfda5325027215dc88723331ebe81279e
Fakhre23 <fakhretamimie@gmail.com>
2025-06-03 10:19:22 +0300

    get start with API

--- app/Http/Controllers/FaqController.php
+++ app/Http/Controllers/FaqController.php
@@ -5,67 +5,51 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Faq;
 
-class FaqController extends Controller
-{
+class FaqController extends Controller {
 
-
-    public function publicIndex()
-    {
+    public function publicIndex() {
         $faqs = Faq::all();
-        return view('faqs.public', compact('faqs'));
+        return view( 'faqs.public', compact( 'faqs' ) );
     }
 
-
-
-
-    public function index()
-    {
+    public function index() {
         $faqs = Faq::all();
-        return view('admin.faq.index', compact('faqs'));
+        return view( 'admin.faq.index', compact( 'faqs' ) );
     }
 
-
-    public function create()
-    {
-        return view('admin.faq.create');
+    public function create() {
+        return view( 'admin.faq.create' );
     }
 
-
-    public function store(Request $request)
-    {
-        $request->validate([
+    public function store( Request $request ) {
+        $request->validate( [
             'question' => 'required',
             'answer' => 'required',
-        ]);
-        Faq::create($request->only('question', 'answer'));
-        return redirect()->route('dashboard');
+        ] );
+        Faq::create( $request->only( 'question', 'answer' ) );
+        return redirect()->route( 'dashboard' );
     }
 
-
-    public function edit($id)
-    {
-        $faqs = Faq::findOrFail($id);
-        return view('admin.faq.edit', compact('faqs'));
+    public function edit( $id ) {
+        $faqs = Faq::findOrFail( $id );
+        return view( 'admin.faq.edit', compact( 'faqs' ) );
     }
 
-    public function update(Request $request, $id)
-    {
-        $request->validate([
+    public function update( Request $request, $id ) {
+        $request->validate( [
             'question' => 'required|string|max:255|unique:faqs,question,' . $id,
             'answer' => 'required|string|max:255|unique:faqs,answer,' . $id,
-        ]);
+        ] );
 
-        Faq::findOrFail($id)->update($request->only('question', 'answer'));
+        Faq::findOrFail( $id )->update( $request->only( 'question', 'answer' ) );
 
-        return redirect()->route('dashboard');
+        return redirect()->route( 'dashboard' );
     }
 
-
-    public function destroy($id)
-    {
-        $faq = Faq::findOrFail($id);
+    public function destroy( $id ) {
+        $faq = Faq::findOrFail( $id );
         $faq->delete();
-        return redirect()->route('dashboard');
+        return redirect()->route( 'dashboard' );
     }
 
 }
--- app/Http/Controllers/ReservedWordController.php
+++ app/Http/Controllers/ReservedWordController.php
@@ -5,64 +5,60 @@ namespace App\Http\Controllers;
 use App\Models\ReservedWord;
 use Illuminate\Http\Request;
 
-class ReservedWordController extends Controller
-{
-    public function list() {                            // to list all ReservedWord
+class ReservedWordController extends Controller {
+    public function list() {
+        // to list all ReservedWord
         $reservedWords = ReservedWord::all();
-        return view('reserved.reservedList', compact('reservedWords'));
+        return view( 'reserved.reservedList', compact( 'reservedWords' ) );
     }
 
-                                                        // to delete ReservedWord
-    public function destroy($id) {
-        $name = ReservedWord::findOrFail($id);
+    // to delete ReservedWord
+
+    public function destroy( $id ) {
+        $name = ReservedWord::findOrFail( $id );
         $name->delete();
-        return redirect()->back()->with('success', 'Reserved Word deleted successfully.');
+        return redirect()->back()->with( 'success', 'Reserved Word deleted successfully.' );
     }
 
-                                                        //Show create ReservedWord form
+    //Show create ReservedWord form
 
     public function create() {
-        return view('reserved.addReservedWord');
+        return view( 'reserved.addReservedWord' );
     }
 
-                                                    //Save the new ReservedWord
-    public function store(Request $request) {
-        $request->validate([
+    //Save the new ReservedWord
+
+    public function store( Request $request ) {
+        $request->validate( [
             'word' => 'required|string|unique:reserved_words,word',  //the word must not already exist in the reserved_words table in the word column.
-        ]);
+        ] );
 
-        ReservedWord::create([
-        //column name
+        ReservedWord::create( [
+            //column name
             'word' => $request->word,
-        ]);
-      
-        return redirect()->route('dashboard')->with('success', 'Reserved word added successfully.');
+        ] );
+
+        return redirect()->route( 'dashboard' )->with( 'success', 'Reserved word added successfully.' );
     }
 
-
-
-    public function edit($id){
-        $reservedWords = ReservedWord::findOrFail($id);
-        return view('reserved.EditReseverd', compact('reservedWords'));
+    public function edit( $id ) {
+        $reservedWords = ReservedWord::findOrFail( $id );
+        return view( 'reserved.EditReseverd', compact( 'reservedWords' ) );
 
     }
 
-
-    public function update(Request $request, $id)
-    {
-        $request->validate([
+    public function update( Request $request, $id ) {
+        $request->validate( [
             'word' => 'required|string|max:255|unique:reserved_words,word,' . $id,
-        ]);
+        ] );
 
-        $reservedWord = ReservedWord::findOrFail($id);
+        $reservedWord = ReservedWord::findOrFail( $id );
         $reservedWord->word = $request->word;
         $reservedWord->save();
 
-        return redirect()->route('dashboard')->with('success', 'Reserved Word updated successfully.');
+        return redirect()->route( 'dashboard' )->with( 'success', 'Reserved Word updated successfully.' );
     }
-    
 
-};
-
-
+}
+;
 
